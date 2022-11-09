892ae24552b741f86c8eac0ee71dd5db1cd7b1ad
Kafui Alordo <kalordo7@gmail.com>
2022-11-09 17:49:54 +0000

    Initialize Project with Webpack

---
+++ src/index.js
@@ -0,0 +1,17 @@
+import "./styles/main.scss";
+import logo from "./assets/Freddys_Logo.svg";
+
+const pumpKin = document.getElementById("logoImg");
+pumpKin.src = logo;
+
+/**
+ * TODO
+ * 1. get input fields
+ * 2. validate them
+ * 3. call login function
+ * 4. pass input values to the login function (API call)
+ * 5. if true, redirect to dashboard
+ * 6. if not true, stay on login page, raise error
+ */
+
+console.log("Initialize Project");
---
+++ src/login.js
@@ -0,0 +1,9 @@
+function login() {
+  const config = {
+    headers: {
+      Accept: "application/json",
+    },
+  };
+
+  const result = fetch("url", config);
+}
