52bd4f328ed1cd1fd457a90555e92bdcbb9369fe
Kafui Alordo <kalordo7@gmail.com>
2022-11-10 09:41:20 +0000

    mend

--- src/login.js
+++ src/login.js
@@ -1,9 +1,20 @@
-function login() {
-  const config = {
-    headers: {
-      Accept: "application/json",
-    },
-  };
+import axios from "axios";
 
-  const result = fetch("url", config);
+async function login(username, password) {
+  try {
+    const response = await axios.post(`https://freddy.codesubmit.io/login`, {
+      username: username,
+      password: password,
+    });
+
+    if (response.status == 200) {
+      return response.data;
+    } else {
+      console.error("Error fetching user data");
+    }
+  } catch (error) {
+    console.error("Error ", error);
+  }
 }
+
+export default login;
